87663de37aa334cd5d0009323a323de304454fde
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-21 11:35:45 +0600

    JS ADDING

---
+++ JS/budget-Cal.js
@@ -0,0 +1,15 @@
+// const incomeTotal = document.getElementById('income-total');
+// const incomeValue = incomeTotal.value;
+// console.log('wfn'incomeValue);
+
+function totalCost(product) {
+    const allItemsCost = document.getElementById(product, '-budget');
+    const allItemsValue = allItemsCost.value;
+    allItemsCost.value = allItemsValue;
+    console
+}
+
+document.getElementById('total-veraity').addEventListener('click', function () {
+    totalCost(Food);
+    console.log(totalCost(Food));
+})
